bbb97b9a28f3d92b87611023ea0d35ee2a154bad
Garima <2412garima@gmail.com>
2025-09-28 14:56:03 +0530

    csrf token

---
+++ src/main/java/com/example/SpringSecurity/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.SpringSecurity.controller;
+
+import com.example.SpringSecurity.entity.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    List<Student> students = new ArrayList<>(List.of(new Student(1, "John", 20),
+            new Student(2, "Jane", 22),
+            new Student(3, "Jack", 21)));
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return students;
+    }
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest req) {
+        return (CsrfToken) req.getAttribute("_csrf");
+    }
+    @PostMapping("/student")
+    public String addStudent(@RequestBody Student student) {
+        students.add(student);
+        return "Student added successfully!";
+    }
+}
